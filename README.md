2d64fe50b909cfe2bca8e6f29b0a47444b733860
anushST <anushervon.s.06@gmail.com>
2024-10-03 11:14:45 +0500

    Changed for abdu

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # landset_nasa
+
+Добавть .env файл в корень проекта. .env - файл у Ануша
+
+Потом
+
+```
+docker-compose up -d --build
+```
+
+Swagger - /api/v1/swagger/ 
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,12 +15,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'hi')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-HOST_NAME = os.getenv('HOST_NAME', 'anush.com')
+HOST_NAME = os.getenv('HOST_NAME')
 
 ALLOWED_HOSTS = [HOST_NAME, 'localhost', '127.0.0.1',]
 
